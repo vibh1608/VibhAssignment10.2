fceb6444a1a33224c6e93173a94fdbb70597e992
vibhor kumar srivastava <31175404+vibh1608@users.noreply.github.com>
2017-11-27 22:07:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/user/vibhassignment102/Details.java
@@ -0,0 +1,18 @@
+package com.example.user.vibhassignment102;
+
+/**
+ * Created by user on 25-11-2017.
+ */
+
+//Class to store the data of students.
+public class Details {
+
+    //String array of titles.
+    static String titleName[] = new String[] {
+            "Layout 1",
+            "Layout 2",
+            "Layout 3",
+            "Layout 4"
+    };
+
+}
